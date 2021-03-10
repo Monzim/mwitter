3cf32083e5c950c25cb43e33a32d3ebbbead8476
Azraf Al Monzim <monzim.linux@gmail.com>
2021-03-10 15:56:29 +0600

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Mwitter
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>
