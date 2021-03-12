17e332defc6232ba548f525d44a84132f5023a27
Azraf Al Monzim <monzim.linux@gmail.com>
2021-03-13 01:41:50 +0600

    Tweet Date Added

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -33,8 +33,8 @@
       </div>
     </div>
     <q-separator class="divider" size="10px" color="grey-2" />
-    <q-list>
-      <q-item class="q-py-md">
+    <q-list separator>
+      <q-item v-for="mweet in mweets" :key="mweet.date" class="q-py-md">
         <q-item-section avatar top>
           <q-avatar size="xl">
             <img
@@ -48,11 +48,9 @@
             ><strong> Azraf Al Monzim </strong>
             <span class="text-grey-7"> @monzim </span>
           </q-item-label>
-          <q-item-label class="mweet-content text-body1">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quasi
-            quia earum. Laboriosam sit modi impedit voluptatem molestiae tenetur
-            illo? Laudantium ut ullam harum, sed doloribus velit ea sequi nemo?
-          </q-item-label>
+          <q-item-label class="mweet-content text-body1">{{
+            mweet.content
+          }}</q-item-label>
           <div class="mweet-icons row justify-between q-mt-sm">
             <q-btn
               flat
@@ -71,22 +69,44 @@
           </div>
         </q-item-section>
 
-        <q-item-section side top> 1 min ago </q-item-section>
+        <q-item-section side top>{{
+          mweet.date | relativeDate
+        }}</q-item-section>
       </q-item>
     </q-list>
   </q-page>
 </template>
   
 <script>
+import { formatDistance } from "date-fns";
+
 export default {
   name: "PageHome",
   data() {
     return {
       neMweetContent: "",
+      mweets: [
+        {
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quasi quia earum. Laboriosam sit modi impedit voluptatem molestiae tenetur illo? Laudantium ut ullam harum, sed doloribus velit ea sequi nemo?",
+          date: 1615576884993,
+        },
+        {
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quasi quia earum. Laboriosam sit modi impedit voluptatem molestiae tenetur illo? Laudantium ut ullam harum, sed doloribus velit ea sequi nemo?",
+          date: 1615576985007,
+        },
+      ],
     };
+  },
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date());
+    },
   },
 };
 </script>
+
 
 <style lang="sass">
 .new-mweet
